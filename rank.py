f2ab054c3354fb2d53e5e68a7b8abbcebe768d0e
mrjax <quesomonos@gmail.com>
2015-02-20 16:51:45 -0600

    Initial work

---
+++ rank.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import sys, random
+
+random.seed()
+
+input = open('input.txt','r')
+
+output = open('output.txt', 'r+')
+
+fullText = input.read()
+itemsList = fullText.split()
+
+for value in itemsList
+	items[value] = 0
+
+def findPairs(text)
+	pairList = []
+
+	for item, rank in text.items():
+		ranks[ranks] = item
+
+	for rank in ranks
+		if 
+
+
+
+while(pair = findPairs(items))
+
+
+
+
+
+output.close()
